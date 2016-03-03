10a69d8cb03ccabe02af3743c52b4c90bddc4653
Logan McDonald <loganmcdona11@gmail.com>
2016-03-03 10:44:08 -0800

    hopefully deploying now

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,4 @@
 Rails.application.config.assets.version = '1.0'
 
-Rails.application.config.assets.precompile += ['sweetalert.css', 'metricsgraphics.css']
+Rails.application.config.assets.precompile += ['sweetalert.css']
+Rails.application.config.assets.precompile += ['metricsgraphics.css']
