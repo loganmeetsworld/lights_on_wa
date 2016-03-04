1c621c5d9586eb039411e6cc94932217ac6466d1
Logan McDonald <loganmcdona11@gmail.com>
2016-03-04 13:32:34 -0800

    changed some css

--- app/assets/javascripts/candidates.js
+++ app/assets/javascripts/candidates.js
@@ -2,11 +2,16 @@
 $(document).ready(function() {
   $('#login').click(function(event){
     swal({
-      title: "LOGIN TO SAVE CANDIDATES",
+      title: "login",
       text: "<div class='btn-group'><a class='btn btn-default disabled'><i class='fa fa-github fa-2x' style='width:30px; height:32px'></i></a><a class='btn btn-default btn-lg' href='/auth/github' style='width:12em'> Sign in with Github</a></div>  <br /><br /><div class='btn-group'><a class='btn btn-info disabled'><i class='fa fa-twitter fa-2x' style='width:30px; height:32px'></i></a><a class='btn btn-info btn-lg' href='/auth/twitter' style='width:12em'> Sign in with Twitter</a></div>",
-      confirmButtonText: "No thanks!",
+      confirmButtonText: "cancel",
       confirmButtonColor: "gray",
       html: true
     });
   });
+});
+
+$("li#clear").click(function(event){
+  $("#visual").html('');
+  $("#content").html('');
 });
