fff34ce94dc31c565d9bc36294ceedc8cec093b6
Logan McDonald <loganmcdona11@gmail.com>
2016-03-05 14:10:39 -0800

    trying to get background image to load

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,3 +2,5 @@ Rails.application.config.assets.version = '1.0'
 
 Rails.application.config.assets.precompile += ['sweetalert.css']
 Rails.application.config.assets.precompile += ['metricsgraphics.css']
+Rails.application.config.assets.precompile += ['washington.jpeg']
+
