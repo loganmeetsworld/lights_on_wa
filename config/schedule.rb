6ffd2f995ef6c12a85a37e107f0d47f10bd09af6
Logan McDonald <loganmcdona11@gmail.com>
2016-03-08 12:44:30 -0800

    updated

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,9 +1,10 @@
 set :output, "#{path}/log/daily_jobs.log"
+env :PATH, ENV['PATH']
 
-every 1.day, :at => '11:20 am' do
-  runner "Candidate.cron_job", environment: :production
+every 1.day, :at => '12:50 pm' do
+  runner "Candidate.cron_job"
 end
 
-every 1.day, :at => '11:22 am' do
-  runner "Contribution.cron_job", environment: :production
+every 1.day, :at => '12:55 pm' do
+  runner "Contribution.cron_job"
 end
