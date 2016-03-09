33c11030101ab077c3a9ab586e785d080d86c304
Logan McDonald <loganmcdona11@gmail.com>
2016-03-09 09:59:49 -0800

    cron is working

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,10 +1,10 @@
 set :output, "#{path}/log/daily_jobs.log"
 env :PATH, ENV['PATH']
 
-every 1.day, :at => '3:20 pm' do
+every 1.day, :at => '1:00 am' do
   runner "Candidate.cron_job"
 end
 
-every 1.day, :at => '3:25 pm' do
+every 1.day, :at => '1:30 am' do
   runner "Contribution.cron_job"
 end
