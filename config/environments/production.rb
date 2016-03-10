27de94bb594201996b9cbd4aa32b90f083010180
Logan McDonald <loganmcdona11@gmail.com>
2016-03-09 16:38:37 -0800

    trying to get caching in production to work

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,7 +55,7 @@ Rails.application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
   # Use a different cache store in production.
-  config.cache_store = :dalli_store, '52.35.31.246', { :expires_in => 1.day, :compress => true }
+  config.cache_store = :dalli_store, 'www.lightsonwa.com', { :expires_in => 1.day, :compress => true }
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
