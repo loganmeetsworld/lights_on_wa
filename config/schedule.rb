1664e567345189205a41ae22d9b4d97df44cc159
Logan McDonald <loganmcdona11@gmail.com>
2016-03-14 10:47:46 -0700

    testing cron

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,10 +1,10 @@
 set :output, "#{path}/log/daily_jobs.log"
 env :PATH, ENV['PATH']
 
-every 1.day, :at => '11:15 am' do
+every 1.day, :at => '10:55 am' do
   runner "Candidate.cron_job"
 end
 
-every 1.day, :at => '11:30 am' do
+every 1.day, :at => '11:00 am' do
   runner "Contribution.cron_job"
 end
