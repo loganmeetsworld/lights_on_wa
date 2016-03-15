e9839fc9ccbf3fdae5eea8ea53b7dc964bd78feb
Logan McDonald <loganmcdona11@gmail.com>
2016-03-15 14:54:54 -0700

    changes to schedule ouput

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,9 +1,9 @@
 set :output, {:error => "#{path}/log/error.log", :standard => "#{path}/log/cron.log"}
 
-every 1.day, :at => '2:40 pm' do
+every 1.day, :at => '3:00 pm' do
   runner "Candidate.cron_job"
 end
 
-every 1.day, :at => '2:45 pm' do
+every 1.day, :at => '3:05 pm' do
   runner "Contribution.cron_job"
 end
