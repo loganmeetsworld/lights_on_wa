11d96d29791bfd915dac272c7a492bc30e5e4a4c
Logan McDonald <loganmcdona11@gmail.com>
2016-03-15 14:59:22 -0700

    testing schedule again

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,9 +1,9 @@
 set :output, {:error => "#{path}/log/error.log", :standard => "#{path}/log/cron.log"}
 
-every 1.day, :at => '3:00 pm' do
+every 10.minutes do
   runner "Candidate.cron_job"
 end
 
-every 1.day, :at => '3:05 pm' do
+every 14.minutes do
   runner "Contribution.cron_job"
 end
