5033b8fa5cbc598d33ba525bcdf390737e301806
Logan McDonald <loganmcdona11@gmail.com>
2016-03-15 17:05:34 -0700

    changing the schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,9 +1,9 @@
 set :output, {:error => "#{path}/log/error.log", :standard => "#{path}/log/cron.log"}
 
-every 1000.minutes do
+every 1.day, :at => '1:00 am' do
   runner "Candidate.cron_job"
 end
 
-every 40.minutes do
+every 1.day, :at => '1:30 am' do
   runner "Contribution.cron_job"
 end
